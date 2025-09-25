e198906b2e228b16b53d65df20388bcc1bbfe8f2
Gabriela <gabriela.carniel@sellflux.com.br>
2025-09-25 15:42:37 -0300

    fix: new video url

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -19,7 +19,7 @@ export const Hero = () => {
 								title="YouTube video player"
 								width="100%"
 								height="100%"
-								src="https://www.youtube.com/embed/gazhQuWCwnQ"
+								src="https://www.youtube.com/embed/J17SYLruJgc"
 								frameBorder="0"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 								allowFullScreen
